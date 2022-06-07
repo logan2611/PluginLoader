9b9fc1cc759ab14f8d827c8b95370d84d0a860f5
Logan Gartner <logan@qtechofficial.com>
2022-06-07 00:15:24 -0600

    Fixed possible weird behaviour with paths in uninstall file, paths no longer hard coded or "deck" account specific

--- dist/uninstall.sh
+++ dist/uninstall.sh
@@ -2,16 +2,15 @@
 
 echo "Uninstalling Steam Deck Plugin Loader..."
 
-HOMEBREW_FOLDER=/home/deck/homebrew
+HOMEBREW_FOLDER="${HOME}/.local/share/homebrew/"
 
 # Disable and remove services
 sudo systemctl disable --now plugin_loader.service > /dev/null
-sudo rm -f /home/deck/.config/systemd/user/plugin_loader.service
+sudo rm -f "${HOME}/.config/systemd/user/plugin_loader.service"
 sudo rm -f /etc/systemd/system/plugin_loader.service
 
 # Remove temporary folder if it exists from the install process
 rm -rf /tmp/plugin_loader
 
 # Cleanup services folder
-sudo rm ${HOMEBREW_FOLDER}/services/PluginLoader
-
+sudo rm "${HOMEBREW_FOLDER}/services/PluginLoader"
