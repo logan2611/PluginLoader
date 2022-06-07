4cdfb78f5683315d91c791c3cfb4ff972bd5a3e5
Logan Gartner <logan@qtechofficial.com>
2022-06-07 00:14:47 -0600

    Fixed possible weird behaviour with paths, paths no longer hard coded in service

--- dist/install_nightly.sh
+++ dist/install_nightly.sh
@@ -4,31 +4,29 @@
 
 echo "Installing Steam Deck Plugin Loader nightly..."
 
-HOMEBREW_FOLDER=/home/deck/homebrew
+HOMEBREW_FOLDER="${HOME}/.local/share/homebrew/"
 
 # Create folder structure
-rm -rf ${HOMEBREW_FOLDER}/services
-sudo -u deck mkdir -p ${HOMEBREW_FOLDER}/services
-sudo -u deck mkdir -p ${HOMEBREW_FOLDER}/plugins
+rm -rf "${HOMEBREW_FOLDER}/services"
+sudo -u $USER mkdir -p "${HOMEBREW_FOLDER}/services"
+sudo -u $USER mkdir -p "${HOMEBREW_FOLDER}/plugins"
 
 # Download latest nightly build and install it
 rm -rf /tmp/plugin_loader
 mkdir -p /tmp/plugin_loader
 curl -L https://nightly.link/SteamDeckHomebrew/PluginLoader/workflows/build/main/Plugin%20Loader.zip --output /tmp/plugin_loader/PluginLoader.zip
 unzip /tmp/plugin_loader/PluginLoader.zip -d /tmp/plugin_loader
-cp /tmp/plugin_loader/PluginLoader ${HOMEBREW_FOLDER}/services/PluginLoader
+cp /tmp/plugin_loader/PluginLoader "${HOMEBREW_FOLDER}/services/PluginLoader"
 rm -rf /tmp/plugin_loader
-chmod +x ${HOMEBREW_FOLDER}/services/PluginLoader
+chmod +x "${HOMEBREW_FOLDER}/services/PluginLoader"
 
-systemctl --user stop plugin_loader 2> /dev/null
-systemctl --user disable plugin_loader 2> /dev/null
-rm -f /home/deck/.config/systemd/user/plugin_loader.service
+systemctl --user disable --now plugin_loader 2> /dev/null
+rm -f "${HOME}/.config/systemd/user/plugin_loader.service"
 
-systemctl stop plugin_loader 2> /dev/null
-systemctl disable plugin_loader 2> /dev/null
+systemctl disable --now plugin_loader 2> /dev/null
 rm -f /etc/systemd/system/plugin_loader.service
 
-cat > /etc/systemd/system/plugin_loader.service <<- EOM
+cat > /etc/systemd/system/plugin_loader.service << EOM
 [Unit]
 Description=SteamDeck Plugin Loader
 
@@ -37,14 +35,13 @@ Type=simple
 User=root
 Restart=always
 
-ExecStart=/home/deck/homebrew/services/PluginLoader
-WorkingDirectory=/home/deck/homebrew/services
+ExecStart=${HOMEBREW_FOLDER}/services/PluginLoader
+WorkingDirectory=${HOMEBREW_FOLDER}/services
 
-Environment=PLUGIN_PATH=/home/deck/homebrew/plugins
+Environment=PLUGIN_PATH=${HOMEBREW_FOLDER}/plugins
 
 [Install]
 WantedBy=multi-user.target
 EOM
 systemctl daemon-reload
-systemctl start plugin_loader
-systemctl enable plugin_loader
+systemctl enable --now plugin_loader
